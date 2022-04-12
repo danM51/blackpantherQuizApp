abd819673d748cabcb3a34a4e640468629d150dc
danM51 <93401577+danM51@users.noreply.github.com>
2022-04-12 15:44:41 -0230

    commit changes game functionality
    *FIXED*
    bugs
    - validity check wether actor is or is not the answer to the question
    - question tab border color remains green or red even after change of selection
    -actor icon border color and width to small

--- app.js
+++ app.js
@@ -20,9 +20,12 @@
 const playBtn = document.getElementById('playBtn');
 const gameContainer = document.getElementById('gameContainer');
 // play END
+const questionTab = document.getElementById('questionTab');
 const timer = document.getElementById('countdown');
 const refreshBtn = document.getElementById('refreshBtn');
 const quote = document.getElementById('quote');
+const nice = document.getElementById('nice');
+const nope = document.getElementById('nope');
 
 // timer END
 
@@ -53,14 +56,14 @@ const playGame = ()=>{
 
 
 // Timer and reset functions here -->
-const startingTime = 20;
+const startingTime = 60;
 let time =  startingTime * 1; 
 
 
 const countdown = ()=>{
     if (time > 0) {
         time--;    
-        if (time>10) {
+        if (time>20) {
             timer.style.color = '#ffd100';
         }else{
              timer.style.color = 'white';  
@@ -105,15 +108,67 @@ const changeQuote = ()=>{
 }
 
 
-// actor onclick/select answer functions here -->
+// actor onclick/hover select answer functions here -->
 
 const openInfoTab = (position, visibility)=>{
     return position.style.visibility = visibility;
 }
+const selectActorCorrect = ()=>{
+ questionTab.style.border = 'solid lightGreen 3px';
+ nice.style.color = 'lightGreen';
+ 
+
+}
+const selectActorIncorrect = ()=>{
+   questionTab.style.border = 'solid red 3px';
+   nope.style.color = 'red';
+}
+const revertInitialColor = ()=>{
+    setInterval(() => {
+        questionTab.style.border = 'solid #c0bfbfce 1px ';
+        nice.style.color = 'rgba(146, 146, 146, 0.89)';
+        nope.style.color = 'rgba(146, 146, 146, 0.89)';
+    }, 3000);
+
+}
+
+// correct answers 
+const chadwickAnswers ={
+        cAnswer1:saidStatements.question1,
+  cAnswer2:saidStatements.question3,
+  cAnswer3:saidStatements.question5,
+    }
+    
+const michaelAnswers ={
+        mAnswer1:saidStatements.question6,
+  mAnswer2:saidStatements.question2,
+}
+const lupitaAnswers ={
+        lAnswer1:saidStatements.question4,
+ 
+    }
+    
+const letitiaAnswers ={
+        letitiaAnswer1:saidStatements.question7,
+  letitiaAnswer2:saidStatements.question8,
+}
+const danaiAnswers ={
+        danaiAnswer1:saidStatements.question10,
+  
+    }
+    
+const danielAnswers ={
+        danielAnswer1:saidStatements.question9,
+  
+}
+
+
 
 // score functions here -->
 
-
+// does selection match the question if so then correct (green) if not then incorrect (red)
+//update the score 
+//add the star
 
 // end of toggling info tab function
 
@@ -134,11 +189,19 @@ playBtn.addEventListener('click', ()=>{
  let decreaseCountdown =  setInterval(countdown, 1000);
      decreaseCountdown;
     
-    
-     
     })
 
+// functions END-------- -------- -------- -------- -------- -------- -------->
 
+
+
+
+// EventListeners START-------- -------- -------- -------- -------- -------- -------->
+if (questionTab.style.border != 'solid #c0bfbfce 1px'||
+    nice.style.color != 'grey'||
+    nope.style.color != 'grey' ) {
+    revertInitialColor()
+}
 // chadwick START
 chadwick.addEventListener('mouseenter', ()=>{
         openInfoTab(actorInfoOne, 'visible');
@@ -147,6 +210,25 @@ chadwick.addEventListener('mouseenter', ()=>{
 chadwick.addEventListener('mouseleave', ()=>{
         openInfoTab(actorInfoOne, 'hidden');
     console.log('left!')
+})
+chadwick.addEventListener('click', ()=>{
+// if answer matches question then correct if not then incorrect (use if statement)
+    if (quote.innerText === chadwickAnswers.cAnswer1 || 
+        quote.innerText === chadwickAnswers.cAnswer2 || 
+        quote.innerText === chadwickAnswers.cAnswer3) {
+        chadwick.style.backgroundColor = 'green';
+        chadwick.style.borderColor = 'lightGreen';
+        selectActorCorrect()
+        revertInitialColor()
+
+            
+    }else{
+        selectActorIncorrect();
+        chadwick.style.borderColor = 'red';
+        revertInitialColor()
+    }
+ 
+    console.log('selected!')
 })
 // chadwick END
 
@@ -159,6 +241,22 @@ michael.addEventListener('mouseenter', ()=>{
 michael.addEventListener('mouseleave', ()=>{
     openInfoTab(actorInfoTwo, 'hidden')
 })
+michael.addEventListener('click', ()=>{
+// if answer matches question then correct if not then incorrect (use if statement)
+    if (quote.innerText === michaelAnswers.mAnswer1 || 
+        quote.innerText === michaelAnswers.mAnswer2 ) {
+           selectActorCorrect();
+           michael.style.backgroundColor = 'green';
+           michael.style.borderColor = 'lightGreen';
+
+    }else{
+        selectActorIncorrect();
+        michael.style.borderColor = 'red';
+
+    }
+ 
+    console.log('selected!')
+})
 // michael END
 
 lupita.addEventListener('mouseenter', ()=>{
@@ -168,6 +266,21 @@ lupita.addEventListener('mouseenter', ()=>{
 lupita.addEventListener('mouseleave', ()=>{
     openInfoTab(actorInfoThree, 'hidden');
     console.log('we are working!')
+})
+lupita.addEventListener('click', ()=>{
+// if answer matches question then correct if not then incorrect (use if statement)
+    if (quote.innerText === lupitaAnswers.lAnswer1) {
+           selectActorCorrect();
+           lupita.style.backgroundColor = 'green';
+           lupita.style.borderColor = 'lightGreen';
+
+    }else{
+        selectActorIncorrect();
+        lupita.style.borderColor = 'red';
+
+    }
+ 
+    console.log('selected!')
 })
 // lupita END
 
@@ -179,6 +292,22 @@ letitia.addEventListener('mouseleave', ()=>{
     openInfoTab(actorInfoFour, 'hidden');
     console.log('we are working!')
 })
+letitia.addEventListener('click', ()=>{
+// if answer matches question then correct if not then incorrect (use if statement)
+    if (quote.innerText === letitiaAnswers.letitiaAnswer1 || 
+        quote.innerText === letitiaAnswers.letitiaAnswer2) {
+           selectActorCorrect();
+           letitia.style.backgroundColor = 'green';
+           letitia.style.borderColor = 'lightGreen';
+
+    }else{
+        selectActorIncorrect();
+        letitia.style.borderColor = 'red';
+
+    }
+ 
+    console.log('selected!')
+})
 // letitia END
 
 danai.addEventListener('mouseenter', ()=>{
@@ -189,6 +318,21 @@ danai.addEventListener('mouseleave', ()=>{
     openInfoTab(actorInfoFive, 'hidden');
     console.log('we are working!')
 })
+danai.addEventListener('click', ()=>{
+// if answer matches question then correct if not then incorrect (use if statement)
+    if (quote.innerText === danaiAnswers.danaiAnswer1) {
+           selectActorCorrect();
+           danai.style.backgroundColor = 'green';
+           danai.style.borderColor = 'lightGreen';
+
+    }else{
+        selectActorIncorrect();
+        danai.style.borderColor = 'red';
+
+    }
+ 
+    console.log('selected!')
+})
 // danai END
 
 daniel.addEventListener('mouseenter', ()=>{
@@ -198,7 +342,23 @@ daniel.addEventListener('mouseenter', ()=>{
 daniel.addEventListener('mouseleave', ()=>{
     openInfoTab(actorInfoSix, 'hidden');
     console.log('we are working!')
-})          
+})  
+daniel.addEventListener('click', ()=>{
+// if answer matches question then correct if not then incorrect (use if statement)
+    if (quote.innerText === danielAnswers.danielAnswer1 ) {
+           selectActorCorrect();
+           daniel.style.backgroundColor = 'green';
+           daniel.style.borderColor = 'lightGreen';
+
+    }else{
+        selectActorIncorrect();
+        daniel.style.borderColor = 'red';
+
+    }
+ 
+    console.log('selected!')
+})        
 // daniel END
+// EventListeners end-------- -------- -------- -------- -------- -------- -------->
 
 console.log('we are live!')
